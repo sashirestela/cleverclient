3b973e88221d318e9ae4449a94e0ce8f95c02318
Sashir Estela <sashirestela@yahoo.com>
2023-10-16 19:49:13 +0000

    Refactoring some classes

--- src/main/java/io/github/sashirestela/cleverclient/CleverClient.java
+++ src/main/java/io/github/sashirestela/cleverclient/CleverClient.java
@@ -2,6 +2,7 @@ package io.github.sashirestela.cleverclient;
 
 import java.net.http.HttpClient;
 import java.util.List;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,27 +10,28 @@ import org.slf4j.LoggerFactory;
 import io.github.sashirestela.cleverclient.http.HttpProcessor;
 import io.github.sashirestela.cleverclient.http.InvocationFilter;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
 @NoArgsConstructor
+@Getter
 public class CleverClient {
   private static Logger logger = LoggerFactory.getLogger(CleverClient.class);
-
-  @NonNull
-  private HttpClient httpClient;
 
   @NonNull
   private String urlBase;
 
   private List<String> headers;
+  private HttpClient httpClient;
+
   private HttpProcessor httpProcessor;
 
   @Builder
-  public CleverClient(HttpClient httpClient, String urlBase, List<String> headers) {
-    this.httpClient = httpClient;
+  public CleverClient(String urlBase, List<String> headers, HttpClient httpClient) {
     this.urlBase = urlBase;
-    this.headers = headers;
+    this.headers = Optional.ofNullable(headers).orElse(List.of());
+    this.httpClient = Optional.ofNullable(httpClient).orElse(HttpClient.newHttpClient());
     this.httpProcessor = new HttpProcessor(this.httpClient, this.urlBase, this.headers);
     logger.debug("CleverClient has been created.");
   }
