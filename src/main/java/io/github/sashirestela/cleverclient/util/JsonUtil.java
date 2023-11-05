51ef20bc7b021427ce7a4f85b25c5e663044ad67
Mariusz Bernacki <consulting@didalgo.com>
2023-11-05 04:59:45 +0100

    Ignore unknown properties when deserializing response from JSON.
    Allows for seamless compatibility with future server API versions, as well as compatibility with Azure OpenAI endpoints that returns additional properties in the response which are not available in OpenAI endpoints.

--- src/main/java/io/github/sashirestela/cleverclient/util/JsonUtil.java
+++ src/main/java/io/github/sashirestela/cleverclient/util/JsonUtil.java
@@ -1,5 +1,6 @@
 package io.github.sashirestela.cleverclient.util;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,20 +8,22 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
 import com.fasterxml.jackson.databind.type.CollectionType;
 
 import io.github.sashirestela.cleverclient.support.CleverClientException;
 
 public class JsonUtil {
-    private static ObjectMapper objectMapper = new ObjectMapper()
-            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    private static final ObjectMapper objectMapperStrict = new ObjectMapper();
+    private static final ObjectReader objectReaderIgnoringUnknown = objectMapperStrict.reader()
+            .without(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 
     private JsonUtil() {
     }
 
     public static <T> String objectToJson(T object) {
         try {
-            return objectMapper.writeValueAsString(object);
+            return objectMapperStrict.writeValueAsString(object);
         } catch (JsonProcessingException e) {
             throw new CleverClientException("Cannot convert the object {0} to Json.", object, e);
         }
@@ -28,16 +31,24 @@ public class JsonUtil {
 
     public static <T> T jsonToObject(String json, Class<T> clazz) {
         try {
-            return objectMapper.readValue(json, clazz);
-        } catch (JsonProcessingException e) {
+            return objectReaderIgnoringUnknown.readValue(json, clazz);
+        } catch (IOException e) {
+            throw new CleverClientException("Cannot convert the Json {0} to class {1}.", json, clazz.getName(), e);
+        }
+    }
+
+    public static <T> T jsonToObjectStrict(String json, Class<T> clazz) {
+        try {
+            return objectMapperStrict.readValue(json, clazz);
+        } catch (IOException e) {
             throw new CleverClientException("Cannot convert the Json {0} to class {1}.", json, clazz.getName(), e);
         }
     }
 
     public static <T> List<T> jsonToList(String json, Class<T> clazz) {
         try {
-            CollectionType listType = objectMapper.getTypeFactory().constructCollectionType(ArrayList.class, clazz);
-            return objectMapper.readValue(json, listType);
+            CollectionType listType = objectReaderIgnoringUnknown.getTypeFactory().constructCollectionType(ArrayList.class, clazz);
+            return objectReaderIgnoringUnknown.forType(listType).readValue(json);
         } catch (JsonProcessingException e) {
             throw new CleverClientException("Cannot convert the Json {0} to List of {1}.", json, clazz.getName(), e);
         }
@@ -45,8 +56,8 @@ public class JsonUtil {
 
     public static <T, U> T jsonToParametricObject(String json, Class<T> clazzT, Class<U> clazzU) {
         try {
-            JavaType javaType = objectMapper.getTypeFactory().constructParametricType(clazzT, clazzU);
-            return objectMapper.readValue(json, javaType);
+            JavaType javaType = objectReaderIgnoringUnknown.getTypeFactory().constructParametricType(clazzT, clazzU);
+            return objectReaderIgnoringUnknown.forType(javaType).readValue(json);
         } catch (JsonProcessingException e) {
             throw new CleverClientException("Cannot convert the Json {0} to class of {1}.", json, clazzT.getName(), e);
         }
