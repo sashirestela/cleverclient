d522eb054a298df6259143a45f011b0187e5f502
Sashir Estela <sashirestela@yahoo.com>
2023-11-13 01:29:10 +0000

    Adding files to replicate issue with multi service

---
+++ src/example/java/io/github/sashirestela/cleverclient/example/jsonplaceholder/AlbumService.java
@@ -0,0 +1,32 @@
+package io.github.sashirestela.cleverclient.example.jsonplaceholder;
+
+import java.util.List;
+
+import io.github.sashirestela.cleverclient.annotation.Body;
+import io.github.sashirestela.cleverclient.annotation.DELETE;
+import io.github.sashirestela.cleverclient.annotation.GET;
+import io.github.sashirestela.cleverclient.annotation.POST;
+import io.github.sashirestela.cleverclient.annotation.PUT;
+import io.github.sashirestela.cleverclient.annotation.Path;
+import io.github.sashirestela.cleverclient.annotation.Query;
+import io.github.sashirestela.cleverclient.annotation.Resource;
+
+@Resource("/albums")
+public interface AlbumService {
+
+    @GET
+    List<Album> readAlbums(@Query("_page") Integer page, @Query("_limit") Integer limit);
+
+    @GET("/{albumId}")
+    Album readAlbum(@Path("albumId") Integer albumId);
+
+    @POST
+    Album createAlbum(@Body Album album);
+
+    @PUT("/{albumId}")
+    Album updateAlbum(@Path("albumId") Integer albumId, @Body Album album);
+
+    @DELETE("/{albumId}")
+    Album deleteAlbum(@Path("albumId") Integer albumId);
+
+}
