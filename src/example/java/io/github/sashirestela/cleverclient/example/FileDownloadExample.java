f0371d386c2102849e8699d72a4daeb413686b14
Sashir Estela <sashirestela@gmail.com>
2025-01-24 04:20:16 +0000

    Handle OkHttp read binary result

--- src/example/java/io/github/sashirestela/cleverclient/example/FileDownloadExample.java
+++ src/example/java/io/github/sashirestela/cleverclient/example/FileDownloadExample.java
@@ -3,6 +3,7 @@ package io.github.sashirestela.cleverclient.example;
 import io.github.sashirestela.cleverclient.CleverClient;
 import io.github.sashirestela.cleverclient.annotation.GET;
 import io.github.sashirestela.cleverclient.annotation.Path;
+import io.github.sashirestela.cleverclient.annotation.Resource;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -20,21 +21,22 @@ public class FileDownloadExample extends AbstractExample {
 
     public void run() throws IOException {
         var cleverClient = CleverClient.builder()
-                .baseUrl("https://via.placeholder.com")
+                .baseUrl("https://en.wikipedia.org")
                 .clientAdapter(clientAdapter)
                 .build();
 
         var imageService = cleverClient.create(ImageService.class);
-        var binaryData = imageService.getImage("92c952");
+        var binaryData = imageService.getImage("wikipedia.png");
         var file = new FileOutputStream("src/test/resources/download.png");
         file.write(binaryData.readAllBytes());
         file.close();
     }
 
+    @Resource("/static/images/icons")
     static interface ImageService {
 
-        @GET("/150/{id}")
-        InputStream getImage(@Path("id") String id);
+        @GET("/{name}")
+        InputStream getImage(@Path("name") String name);
 
     }
 
